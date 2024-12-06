154c18fa1fe3648d203881a6329614c6d9d16812
pabrmt <ssetelepabrmt@gmail.com>
2024-12-06 19:42:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  alert(`Login attempted with username: ${username}`);
+});
